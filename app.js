9bf1e8604c02a01786faf86f06e6ebe6dc8301bf
DebdutBiswas <rupam.biswas@outlook.com>
2022-02-24 05:43:22 +0530

    Added Middlewares and Routes

--- app.js
+++ app.js
@@ -1,27 +1,69 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('./configs/database');
+
+let authMiddleware = require('./middlewares/auth');
+
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
+const visitorsRouter = require('./routes/visitors');
+const logsRouter = require('./routes/logs');
+
+db.authenticate().then(() => {
+    console.log('Database connected...');
+    // (async () => await db.sync())();
+}).catch(err => {
+    console.log('Error: ' + err);
+    process.exit();
+});
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+// Prevent public stack traces on production
+process.env.NODE_ENV === 'production' ? app.set('env', 'production') : app.set('env', 'development');
+
 // Mddlewares
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors({
     origin: '*'
 }));
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+            res.status(400).send({
+                message: 'Malformed request body has been sent!'
+        });
+    } else next();
+});
 
 // Default routes
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to VES API"});
 });
 
+// Auth route
+app.use('/auth', authRouter);
+
+// Auth middlewares
+// app.use(authMiddleware);
+
+// Bypass auth middlewares (debug)
+authMiddleware = (req, res, next) => { next() };
+
+// App routes
+app.use('/users', authMiddleware, usersRouter);
+app.use('/visitors', authMiddleware, visitorsRouter);
+app.use('/logs', authMiddleware, logsRouter);
+
 // Invalid routes
 app.use('/*', (req, res)=>{
     return res.status(404).json({
         error : true,
-        message : "404 Not found!"
+        message : '404 Not found!'
     })
 });
 
