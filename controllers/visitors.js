e5a50a82f8191d556334137d038e64e7a97fe484
DebdutBiswas <rupam.biswas@outlook.com>
2022-02-25 13:14:15 +0530

    Added Checkout Controller Method

--- controllers/visitors.js
+++ controllers/visitors.js
@@ -5,7 +5,7 @@
 // router.post('/search', visitorsController.searchVisitorByParams);
 // router.post('/checkout', visitorsController.checkoutVisitor);
 
-const { Op } = require('sequelize');
+const { Op, literal } = require('sequelize');
 const db = require('../configs/database');
 const { checkDateFormat, getISOTimeStamp, getISODate } = require('../utils/timelib');
 const initModels = require('../models/initModels');
@@ -138,5 +138,92 @@ exports.searchVisitorByParams = async (req, res) => {
 };
 
 exports.checkoutVisitor = async (req, res) => {
+    const {
+        id,
+        checkout_done,
+        out_time
+    } = {
+        id: req.body?.id ?? req.body.id ? req.body.id : 0,
+        checkout_done: 1,
+        out_time: checkDateFormat(req.body?.out_time?.trim?.() ?? '') ? getISODate(req.body.out_time.trim?.()) : getISOTimeStamp()
+    };
 
+    if (id === 0) {
+        return res.status(400).send({
+            message: 'Please upload valid JSON format!'
+        });
+    }
+
+    console.log({id, out_time});
+
+    logSearchParams = {
+        id,
+        checkout_done: 0
+    };
+
+    await logsModel.update({
+        checkout_done,
+        out_time
+    }, {where: logSearchParams}, { fields: ['checkout_done', 'out_time']})
+    .then(async logUpdateQueryResult => {
+        [ logUpdateResult ] = logUpdateQueryResult;
+        console.log(logUpdateResult);
+
+        if (!logUpdateResult) {
+            res.status(500).send({
+                message: 'The log does not exist or already checked out!'
+            });
+        } else {
+            await logsModel.findOne({where: {id}, include: [{model: visitorsModel, as: 'visitor_info', where: {}, attributes: {exclude: ['id']}}]})
+            .then(async log => {
+                if (log !== null) {
+                    await visitorsModel.update({
+                        checkout_count: literal('checkout_count + 1'),
+                        last_checkout_time: out_time
+                    }, {where: {id: log.dataValues.visitor_id}}, { fields: ['checkout_done', 'out_time']})
+                    .then(visitorUpdateQueryResult => {
+                        [ visitorUpdateResult ] = visitorUpdateQueryResult;
+
+                        if (!visitorUpdateResult ) {
+                            res.status(500).send({
+                                message: 'The visitor does not exist or already checked out!'
+                            });
+                        } else {
+                            res.send({
+                                'data': {
+                                    ...log.dataValues,
+                                    visitor_info: {
+                                        ...log.dataValues.visitor_info.dataValues,
+                                        checkout_count: (+log.dataValues.visitor_info.dataValues.checkout_count + 1),
+                                        last_checkout_time: out_time
+                                    }
+                                }
+                            });
+                        }
+                    })
+                    .catch(err => {
+                        res.status(500).send({
+                            data: {...log.dataValues},
+                            message: err.message || 'Something went wrong while updating visitor!'
+                        });
+                    });
+                }
+                else {
+                    res.status(500).send({
+                        message: 'The log does not exist!'
+                    });
+                }
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || 'Something went wrong while getting the updated log!'
+                });
+            });
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || 'Something went wrong while updating log!'
+        });
+    });
 };
